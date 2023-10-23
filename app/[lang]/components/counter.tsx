c820487f37ee9a1369d75f56b56b6dc43a2b4d12
MinHeum <plutokor@gmail.com>
2023-10-23 23:37:49 +0900

    first commit

--- app/[lang]/components/counter.tsx
+++ app/[lang]/components/counter.tsx
@@ -1,26 +1,37 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
+import { useState } from "react";
+
+import {
+  counterSlice,
+  useSelector,
+  useDispatch,
+  selectCount,
+  incrementAsync,
+  incrementIfOddAsync,
+} from "@/lib/redux";
 
 export default function Counter({
   dictionary,
 }: {
   dictionary: {
-    increment: string
-    decrement: string
-  }
+    increment: string;
+    decrement: string;
+  };
 }) {
-  const [count, setCount] = useState(0)
+  const dispatch = useDispatch();
+  const count = useSelector(selectCount);
+
   return (
     <p>
       This compoment is rendered on client:
-      <button onClick={() => setCount((n) => n - 1)}>
+      <button onClick={() => dispatch(counterSlice.actions.decrement())}>
         {dictionary.decrement}
       </button>
       {count}
-      <button onClick={() => setCount((n) => n + 1)}>
+      <button onClick={() => dispatch(counterSlice.actions.increment())}>
         {dictionary.increment}
       </button>
     </p>
-  )
+  );
 }
